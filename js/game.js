7632d41672f141069422cb4cf4df536cb0a9bc84
aaruesuu <aaruesuu@gmail.com>
2026-01-11 20:09:31 +0900

    Update game.js

--- js/game.js
+++ js/game.js
@@ -50,14 +50,14 @@ import {
 
 // === 正解ポケモン固定 ===
 const DEBUG_FIXED_ANSWER = true;
-const DEBUG_FIXED_NAME = 'ポニータ（ガラルのすがた）';
-const DEBUG_FIXED_ID = 10162;
+const DEBUG_FIXED_NAME = 'オンバット';
+const DEBUG_FIXED_ID = 714;
 // =======================
 
 // === ランダムスタート初回固定 ===
 const DEBUG_FIXED_RANDOM_START = true;
-const DEBUG_FIXED_RANDOM_START_NAME = 'メテノ（りゅうせいのすがた）';
-const DEBUG_FIXED_RANDOM_START_ID = 774;
+const DEBUG_FIXED_RANDOM_START_NAME = 'カイデン';
+const DEBUG_FIXED_RANDOM_START_ID = 940;
 // ================================
 
 let gameMode = null;
